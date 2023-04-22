a7b2e5a94bef87c76102160656b19396c257688a
ttyyjjkkoott <t@tyjkot.io>
2023-04-21 19:22:16 -0500

    add profile pic / initial mod

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import profilePicture from './assets/profilePic.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img
+        src={profilePicture}
+        alt="tyjkot profile picture"
+        className="profile-picture"
+      />
+      <div className="buttons">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/tyjkot"
           target="_blank"
           rel="noopener noreferrer"
+          className="button twitter"
         >
-          Learn React
+          <i className="fa fa-twitter"></i>
         </a>
-      </header>
+        <a
+          href="https://github.com/ttyyjjkkoott"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button github"
+        >
+          <i className="fa fa-github"></i>
+        </a>
+        <a
+          href="mailto:t@tyjkot.io"
+          className="button email"
+        >
+          <i className="fa fa-envelope"></i>
+        </a>
+      </div>
     </div>
   );
 }
