60d62803ff9c652e836543c4bdd0021e776a86d0
ttyyjjkkoott <t@tyjkot.io>
2023-04-29 22:27:36 -0500

    fb file add

---
+++ src/redux/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5fb2uAUEYiABVSj1-bxLwTg3lqE6vDwc",
+    authDomain: "syte-message-board.firebaseapp.com",
+    projectId: "syte-message-board",
+    storageBucket: "syte-message-board.appspot.com",
+    messagingSenderId: "264207345589",
+    appId: "1:264207345589:web:d66ebfeecb9bd3d0ae9bfc",
+    measurementId: "G-LJ75WY22CE"
+};
+
+export default firebaseConfig;
